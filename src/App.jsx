fa696536b4e6fe05dd84dcf8c0e754b20e13efe1
MarceloCaballeroo <marc.caballero@duocuc.cl>
2024-02-23 12:07:20 -0300

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Marcelo Caballero",
+  title: "Data Analysis & Cloud Computing",
+  email: "marcelo.caballeroolave@gmail.com",
+  gitHub: "MarceloCaballeroo",
+  instagram: "",
+  linkedIn: "marcelo-caballero-olave",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
